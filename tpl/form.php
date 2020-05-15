52bd6219f32bf0c88d714fefab645baa04291b18
Sergeygeek <sukhorukov1986@yandex.ru>
2020-05-15 15:07:19 +0400

    add parser files

---
+++ tpl/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <form action="parser.php" method="post">
+    <input type="text" name="link" id="link"><br>
+    <input type="submit" value="Спарсить">
+  </form>
+  <script src="index.js"></script>
+  <script src="formHandling.js"></script>
+</body>
+</html>
